3c4c34e412c8ca86beedf57e466e2536c20801b9
L-Fortunato <lucasfortunatolima@protonmail.com>
2020-08-26 23:40:21 -0300

    Adding Starter Files

---
+++ script.js
@@ -0,0 +1,78 @@
+function toggleAbt(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal-about');
+    modal.classList.toggle('display-modal');
+}
+function toggle1(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal1');
+    modal.classList.toggle('display-modal');
+}
+function toggle2(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal2');
+    modal.classList.toggle('display-modal');
+}
+function toggle3(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal3');
+    modal.classList.toggle('display-modal');
+}
+function toggle4(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal4');
+    modal.classList.toggle('display-modal');
+}
+function toggle5(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal5');
+    modal.classList.toggle('display-modal');
+}
+function toggle6(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal6');
+    modal.classList.toggle('display-modal');
+}
+function toggle7(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal7');
+    modal.classList.toggle('display-modal');
+}
+function toggle8(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal8');
+    modal.classList.toggle('display-modal');
+}
+function toggle9(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal9');
+    modal.classList.toggle('display-modal');
+}
+function toggle10(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal10');
+    modal.classList.toggle('display-modal');
+}
+function toggle11(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal11');
+    modal.classList.toggle('display-modal');
+}
+function toggle12(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const modal = document.getElementById('modal12');
+    modal.classList.toggle('display-modal');
+}
